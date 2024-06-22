6b2deb8093c9bd917b5f11e110e5690e18963179
JMezaV2020 <112105015+JMezaV2020@users.noreply.github.com>
2024-06-22 16:55:52 -0400

    cambios

--- myproject/app.py
+++ myproject/app.py
@@ -1,6 +1,28 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
+
+def procesar_datos():
+    with open('datos.json', encoding='utf-8') as f:
+        data = json.load(f)
+    processed_data = []
+    for item in data:
+        for key, value in item.items():
+            fields = value.split(';')
+            processed_data.append({
+                'anio': fields[0],
+                'mes': fields[1],
+                'region': fields[2],
+                'comuna': fields[3],
+                'tipo_clientes': fields[4],
+                'tarifa': fields[5],
+                'clientes_facturados': fields[6],
+                'e1_kwh': fields[7],
+                'e2_kwh': fields[8],
+                'energia_kwh': fields[9],
+            })
+    return processed_data
 
 # Ruteos y vistas
 @app.route('/')
@@ -11,13 +33,14 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/datos')
-def datos():
-    return render_template('datos.html')
-
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/datos')
+def datos():
+    data = procesar_datos()
+    return render_template('datos.html', datos=data)
 
 # Configuración de archivo estático CSS
 @app.route('/static/<path:path>')
